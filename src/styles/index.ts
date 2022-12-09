3dae76f5a89863461275f3bc51f8efa59dae4e4a
Daniel Cazé <danielctlima@gmail.com>
2022-12-08 21:22:13 -0300

    Commit 03: Configurando Stitches

---
+++ src/styles/index.ts
@@ -0,0 +1,18 @@
+import { createStitches } from '@stitches/react'
+
+export const {
+  config,
+  createTheme,
+  css,
+  getCssText,
+  globalCss,
+  keyframes,
+  styled,
+  theme
+} = createStitches({
+  theme: {
+    colors: {
+      rocketseat: '#8257e6',
+    }
+  }
+})
