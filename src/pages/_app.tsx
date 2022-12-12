9d30511e760e07d3e210ad36674697097475f7c4
Daniel Cazé <danielctlima@gmail.com>
2022-12-12 10:44:27 -0300

    Commit 05: Cabeçalho da aplicação

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,17 @@
 import type { AppProps } from 'next/app'
+import logoImg from '../assets/logo.svg'
 import { globalStyles } from '../styles/global'
+import { Container, Header } from '../styles/pages/app'
 
 globalStyles()
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Container>
+      <Header>
+        <img src={logoImg.src} alt="" />
+      </Header>
+      <Component {...pageProps} />
+    </Container>
+  )
 }
