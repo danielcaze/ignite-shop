70693f040ffbe662f668bf9a6bc5da70e62ed2db
Daniel Cazé <danielctlima@gmail.com>
2022-12-13 11:40:34 -0300

    Commit 05: Imagens no Nextjs

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,8 @@
 import type { AppProps } from 'next/app'
 import logoImg from '../assets/logo.svg'
-import { globalStyles } from '../styles/global'
+import Image from 'next/image'
 import { Container, Header } from '../styles/pages/app'
+import { globalStyles } from '../styles/global'
 
 globalStyles()
 
@@ -9,7 +10,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <Container>
       <Header>
-        <img src={logoImg.src} alt="" />
+        <Image src={logoImg} alt="" />
       </Header>
       <Component {...pageProps} />
     </Container>
