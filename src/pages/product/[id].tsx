6433bde00ff495635a4915a3f5ef089a538bdbea
Daniel Cazé <danielctlima@gmail.com>
2022-12-14 12:11:18 -0300

    Commit 13: Estrutura pagina de produtos

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,5 +1,20 @@
+import Image from "next/image";
+import { ImageContainer, ProductContaienr, ProductDetails } from "../../styles/pages/product";
+
 export default function Product() {
   return (
-    <h1>Product</h1>
+    <ProductContaienr>
+      <ImageContainer>
+        {/* <Image /> */}
+      </ImageContainer>
+      <ProductDetails>
+        <h1>Camiseta X</h1>
+        <span>R$ 79,90</span>
+
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus debitis illum, asperiores eveniet magni doloribus maxime, voluptatem rem error dicta eligendi illo quisquam laudantium incidunt eum dolore perspiciatis? Explicabo, fugit!</p>
+
+        <button>Comprar agora</button>
+      </ProductDetails>
+    </ProductContaienr>
   )
 }
