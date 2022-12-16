8fb88f11fbcdacb8832885a299a8ba4aef5917b7
Daniel Cazé <danielctlima@gmail.com>
2022-12-15 21:26:16 -0300

    Commit 18: Tela de sucesso

---
+++ src/styles/pages/success.ts
@@ -0,0 +1,57 @@
+import { styled } from "..";
+
+export const SuccessContainer = styled('main', {
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  alignItems: 'center',
+
+  marginInline: 'auto',
+  height: 656,
+
+  h1: {
+    fontSize: '$2xl',
+    color: '$gray100'
+  },
+
+  p: {
+    fontSize: '$xl',
+    color: '$gray300',
+    maxWidth: 560,
+    textAlign: 'center',
+    marginTop: '2rem',
+    lineHeight: 1.4
+  },
+
+  a: {
+    display: 'block',
+    marginTop: '5rem',
+
+    fontSize: '$lg',
+    color: '$green500',
+    textDecoration: 'none',
+    fontWeight: 'bold',
+
+    '&:hover': {
+      color: '$green300'
+    }
+  }
+})
+
+export const ImageContainer = styled('div', {
+  width: '100%',
+  maxWidth: 130,
+  height: 145,
+  background: 'linear-gradient(100deg, #1ea483 0%, #7455d4 100%)',
+  borderRadius: 8,
+  padding: '0.25rem',
+  marginTop: '4rem',
+
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+
+  img: {
+    objectFit: 'cover'
+  }
+})
